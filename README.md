fec66f2c202d154ffe41946e24f43c8d03eafb27
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2019-03-24 03:54:13 +0800

    Added docs and also edited gitignore and also official release

---
+++ README.md
@@ -0,0 +1,14 @@
+## Documentation
+
+### Usage: `python3 <filename (ends with py)> <input_image> <output_image_with_jpg_or_png>`
+
+-   Example:
+    -   python3 main.py input.jpg output.jpg
+    -   python3 main.py input.png output.png
+
+-   Wrong:
+    -   python3 main.py input.jpg output.png
+        -   Both not in the same file format
+
+## What Happens?
+>   It will spew out an image with all the approximation of boxes where the letters are detected, basically, you are seeing it in the computer's eye
--- main.py
+++ main.py
@@ -3,24 +3,32 @@ import pytesseract
 from PIL import ImageDraw, Image
 from sys import argv
 
+# Parameters used for in the program later on
 image_param = argv[1]
 output_param = argv[2]
 
 # Variable for image
-im = Image.open('test3.jpeg')
+im = Image.open(image_param)
 draw = ImageDraw.Draw(im)
 boxes = pytesseract.image_to_boxes(im)
 
-splitl = boxes.splitlines()
-seperated = []
 
+# PRINT DETECTED TEXTS
 print(pytesseract.image_to_string(im))
 
-for l in splitl:
+# ARRAY FOR SEPERATED TEXTS THAT SPEW OUT THE COORDS: 199 2899 199 29 0
+# WHICH IS X1 X2 Y1 Y2 AND THE LAST ONE, IDK
+seperated = []
+for l in boxes.splitlines():
     # print(l[2:].split(' '))
     seperated.append(l[2:].split(' '))
 
+# FOR EACH COORDS IN THE SEPERATED LIST
+# COORDS[0] IS X1
+# COORDS[1] IS X2
+# AND SO ON
 for coords in seperated:
     draw.rectangle([int(coords[0]), int(coords[1]), int(coords[2]), int(coords[3])], outline='rgb(255,0,0)')
 
-im.save('./outputnew.jpg')
+# WHEN DONE, SAVE
+im.save(output_param)
